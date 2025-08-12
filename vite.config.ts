8c8633befb0ed9aa83147b248cdbde0c02bd0544
irwanx <irwan080304@gmail.com>
2025-08-12 15:00:33 +0700

    fix: final deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  // base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteSingleFile()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
